3d54b492b0b514d135c310cdf788f7fc3f69899d
Neeraj Maurya <neerajxp@gmail.com>
2021-02-15 12:22:37 -0500

    commit

--- src/app/coinbinance/coinbinance-app.component.ts
+++ src/app/coinbinance/coinbinance-app.component.ts
@@ -3,7 +3,7 @@ import { ToolbarComponent } from './components/toolbar/toolbar.component';
 @Component({
   selector: 'app-coinbinance-app',
   templateUrl: './coinbinance-app.component.html',  
-  styleUrls: ['./coinbinance-app.component.scss']
+  styleUrls:  ['./coinbinance-app.component.scss']
 })
 export class CoinbinanceAppComponent implements OnInit {
 
