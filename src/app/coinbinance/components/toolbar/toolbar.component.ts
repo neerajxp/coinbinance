b1953d827d5b08ce5f876f233a1afff46fab128a
Neeraj Maurya <neerajxp@gmail.com>
2021-02-15 23:08:27 -0500

    layout

--- src/app/coinbinance/components/toolbar/toolbar.component.ts
+++ src/app/coinbinance/components/toolbar/toolbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ThemePalette} from '@angular/material/core';
 
 @Component({
   selector: 'app-toolbar',
@@ -6,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent implements OnInit {
+  links = ['First', 'Second', 'Third'];
+  activeLink = this.links[0];
+  background: ThemePalette = undefined;
+
+  toggleBackground() {
+    this.background = this.background ? undefined : 'primary';
+  }
+
+  addLink() {
+    this.links.push(`Link ${this.links.length + 1}`);
+  }
 
   constructor() { }
 
