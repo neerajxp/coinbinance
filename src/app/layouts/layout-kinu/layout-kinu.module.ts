3bc44468019c9491567f1d6de7baf1af73703027
Neeraj Maurya <neerajxp@gmail.com>
2021-06-14 21:57:53 -0400

    kinu blog added

--- src/app/layouts/layout-kinu/layout-kinu.module.ts
+++ src/app/layouts/layout-kinu/layout-kinu.module.ts
@@ -6,9 +6,10 @@ import { BlogComponent } from 'src/app/pages/learn/blog/blog.component';
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from 'src/app/shared/modules/material.module'
+import { ACryptoForHereComponent } from 'src/app/pages/kinu/blog/a-crypto-for-here/a-crypto-for-here.component';
 
 @NgModule({
-  declarations: [LayoutKinuComponent,BlogComponent],
+  declarations: [LayoutKinuComponent,BlogComponent,ACryptoForHereComponent],
   imports: [
     CommonModule, SharedModule,RouterModule,FlexLayoutModule,MaterialModule
   ]
--- src/app/layouts/layout-learn/layout-learn.module.ts
+++ src/app/layouts/layout-learn/layout-learn.module.ts
@@ -5,6 +5,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from 'src/app/shared/modules/material.module';
+ 
 @NgModule({
   declarations: [LayoutLearnComponent],
   imports: [CommonModule, SharedModule,RouterModule,FlexLayoutModule,MaterialModule]
